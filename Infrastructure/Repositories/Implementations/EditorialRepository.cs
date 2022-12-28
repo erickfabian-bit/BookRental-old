eb007188f1e81af5e566d49523d54b382a3c82aa
erick-bit <eribamill@gmail.com>
2022-12-28 10:28:35 -0500

    Add Editorial Repository - Method finfAll

---
+++ Infrastructure/Repositories/Implementations/EditorialRepository.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using Infrastructure.Context;
+using Infrastructure.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Implementations
+{
+    public class EditorialRepository: IEditorialRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public EditorialRepository(ApplicationDBContext context) 
+        {
+            _context = context;
+        }
+        public async Task<IList<Editorial>> findAll()
+        => await _context.Editoriales.OrderBy(e => e.Id).ToListAsync();
+    }
+}
