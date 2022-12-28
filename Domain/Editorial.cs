eaabc7f1eed5cc00f2a14622e79740eca52306e3
erick-bit <eribamill@gmail.com>
2022-12-28 09:20:20 -0500

    Add model editorial

---
+++ Domain/Editorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Editorial
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+        public int Estado { get; set; }
+
+
+    }
+}
