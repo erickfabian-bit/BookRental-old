832630609d12a0598e8d45838ad40b542f494f4f
erick-bit <eribamill@gmail.com>
2022-12-28 11:23:27 -0500

    Editorial controller - list method

---
+++ WebApi/Controllers/EditorialController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Dtos.Editoriales;
+using Application.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EditorialController : Controller
+    {
+        private readonly IEditorialService _editorialService;
+
+        public EditorialController(IEditorialService editorialService)
+        {
+            _editorialService = editorialService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<EditorialDto>> Get()
+        => await _editorialService.FindAll();
+    }
+}
