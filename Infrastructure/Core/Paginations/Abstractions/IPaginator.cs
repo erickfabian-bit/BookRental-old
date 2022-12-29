46194bf8856376fc9c3493d5c54915bbb2aca726
erick-bit <eribamill@gmail.com>
2022-12-28 19:05:50 -0500

    Add core pagination

---
+++ Infrastructure/Core/Paginations/Abstractions/IPaginator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Paginations;
+
+namespace Infrastructure.Core.Paginations.Abstractions
+{
+    public interface IPaginator<T> 
+    {
+        Task<ResponsePagination<T>> Paginate(IQueryable<T> query, RequestPagination<T> request)
+    }
+}
---
+++ Infrastructure/Core/Paginations/Implementations/Paginator.cs
@@ -0,0 +1,42 @@
+﻿using Infrastructure.Core.Paginations.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Paginations;
+
+namespace Infrastructure.Core.Paginations.Implementations
+{
+    public class Paginator<T> : IPaginator<T>
+    {
+        public Paginator() { }
+
+        public async Task<ResponsePagination<T>> Paginate(IQueryable<T> query, RequestPagination<T> request)
+        {
+            var total = await query.CountAsync();
+            var pagination = new Pagination(total, request.Page, request.PerPage);
+
+            var sizePerPage = pagination.PerPage;
+
+            var diference = (pagination.To - pagination.From) + 1;
+
+            if (diference < pagination.PerPage) sizePerPage = diference;
+
+            var currentPage = pagination.CurrentPage;
+
+            if (currentPage > 0) currentPage = pagination.CurrentPage - 1;
+
+            query = query.Skip(currentPage * sizePerPage).Take(sizePerPage);
+            var data  = await query.ToListAsync();
+
+            var response = new ResponsePagination<T>(pagination)
+            { 
+                Data = data
+            };
+
+            return response;
+        }
+    }
+}
